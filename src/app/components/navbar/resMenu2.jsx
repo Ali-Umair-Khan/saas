b44dee1b4493e73283c849781d63e0d0016b419a
Ali-Umair-Khan <rexexpert@gmail.com>
2023-11-20 21:43:38 +0500

    changes in navbar

--- src/app/components/navbar/resMenu2.jsx
+++ src/app/components/navbar/resMenu2.jsx
@@ -1,5 +1,7 @@
+'use client'
 import Link from 'next/link';
 import './style4.css';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faWindowMinimize,
@@ -8,16 +10,28 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const ResponsiveMenu = () => {
+    
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleMenuItemClick = () => {
+    setMenuOpen(false); // Close the menu when a menu item is clicked
+  };
+
+
     return(
         <nav>
             <ul className='navbar'>
-                <li className='navbar__logo'><Link href='/'><img src='X__logoA.png' alt="" /></Link></li>
-                <input type='checkbox' id='check' className='navbar__checkbox'/>
+                <li onClick={handleMenuItemClick} className='navbar__logo'><Link href='/'><img src='X__logoA.png' alt="" /></Link></li>
+                <input type='checkbox' id='check' className='navbar__checkbox' checked={menuOpen} onChange={handleMenuToggle}/>
                 <span className='navbar__menu'>
-                    <li><Link href='/'>Home</Link></li>
-                    <li><Link href='/'>Docs</Link></li>
-                    <li><Link href='/'>Pricing</Link></li>
-                    <li><Link href='/'>Contact <FontAwesomeIcon icon={faUser} style={{ fontSize: '1.5rem', marginLeft:'0.5rem'}} /></Link></li>
+                    <li onClick={handleMenuItemClick}><Link href='/'>Home</Link></li>
+                    <li onClick={handleMenuItemClick}><Link href='/'>Docs</Link></li>
+                    <li onClick={handleMenuItemClick}><Link href='/'>Pricing</Link></li>
+                    <li onClick={handleMenuItemClick}><Link href='/'>Contact <FontAwesomeIcon icon={faUser} style={{ fontSize: '1.5rem', marginLeft:'0.5rem'}} /></Link></li>
                     <label for='check' className='navbar__close'><FontAwesomeIcon icon={faWindowMinimize} style={{ fontSize: '3rem', color:'white'}}/></label>
                 </span>
                 <label for='check' className='navbar__open'><FontAwesomeIcon icon={faBars} style={{ fontSize: '3rem' }}/></label>
